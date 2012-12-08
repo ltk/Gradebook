5924ca2491f54b58f92eb9c295a273af54946dea
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-12-07 19:57:00 -0500

    rearranging partials and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  def grades_hash
-    { "A+" => 4.30, "A" => 4.00, "A-" => 3.70, "B+" => 3.30, "B" => 3.00, "B-" => 2.70, "C+" => 2.30, "C" => 2.00, "C-" => 1.70, "D+" => 1.30, "D" => 1.00, "D-" => 0.70, "F" => 0.00 }
-  end
+  # def grades_hash
+  #   { "A+" => 4.30, "A" => 4.00, "A-" => 3.70, "B+" => 3.30, "B" => 3.00, "B-" => 2.70, "C+" => 2.30, "C" => 2.00, "C-" => 1.70, "D+" => 1.30, "D" => 1.00, "D-" => 0.70, "F" => 0.00 }
+  # end
 
-  def grade_to_gpa(grade)
-    grades_hash[grade]
-  end
+  # def grade_to_gpa(grade)
+  #   grades_hash[grade]
+  # end
 
-  def gpa_to_grade(gpa)
-    gpa ? grades_hash.invert[gpa.to_f] : nil
-  end
+  # def gpa_to_grade(gpa)
+  #   gpa ? grades_hash.invert[gpa.to_f] : nil
+  # end
 
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
@@ -20,33 +20,33 @@ module ApplicationHelper
     link_to(name, '#', class: "add_fields button", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
-  def gpa_to_label_class(gpa)
-    case gpa
-      when 0..0.7
-        'inverse'
-      when 0.7..1.3
-        'important'
-      when 1.7..2.7
-        'warning'
-      when 2.7..3.7
-        'info'
-      else
-        'success'
-    end
-  end
+  # def gpa_to_label_class(gpa)
+  #   case gpa
+  #     when 0..0.7
+  #       'inverse'
+  #     when 0.7..1.3
+  #       'important'
+  #     when 1.7..2.7
+  #       'warning'
+  #     when 2.7..3.7
+  #       'info'
+  #     else
+  #       'success'
+  #   end
+  # end
 
-  def grade_to_label_class(grade)
-    case grade
-      when "A+", "A", "A-"
-        'success'
-      when "B+", "B", "B-"
-        'info'
-      when "C+", "C", "C-"
-        'warning'
-      when "D+", "D", "D-"
-        'important'
-      else
-        'inverse'
-    end
-  end
+  # def grade_to_label_class(grade)
+  #   case grade
+  #     when "A+", "A", "A-"
+  #       'success'
+  #     when "B+", "B", "B-"
+  #       'info'
+  #     when "C+", "C", "C-"
+  #       'warning'
+  #     when "D+", "D", "D-"
+  #       'important'
+  #     else
+  #       'inverse'
+  #   end
+  # end
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,5 @@
 module CoursesHelper
   def grade_selector(form_builder, label = 'Grade')
-    form_builder.input :grade, :collection => grades_hash, :label => label
+    form_builder.input :grade, :collection => Grade.letter_grade_hash, :label => label
   end
 end
